9dfa3db560d112498434834ea8bcbb4bc147406f
alisson-lucas <lucasoliveira.alisson@gmail.com>
2025-04-01 17:58:03 -0300

    main | some adjusts

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,25 +8,25 @@ export default function Features() {
                         <article className="flex flex-col items-center justify-center lg:justify-start w-full lg:w-[calc(100%/4-30px)] lg:min-h-[308px] gap-10 py-10 p-4 bg-[#353535] hover:bg-[#234689] rounded-3xl">
                             <div className="flex flex-col items-start justify-center w-full 2xl:px-10 gap-8">
                                 <span className="text-xl text-white font-semibold">Ensino Confessional</span>
-                                <p>O nosso seminário é comprometido com uma educação reformada. Ser Cristão é ser parte daquilo que o Espírito ensino ao longo da história da igreja.</p>
+                                <p className="text-white">O nosso seminário é comprometido com uma educação reformada. Ser Cristão é ser parte daquilo que o Espírito ensino ao longo da história da igreja.</p>
                             </div>
                         </article>
                         <article className="flex flex-col items-center justify-center lg:justify-start w-full lg:w-[calc(100%/4-30px)] lg:min-h-[308px] gap-10 py-10 p-4 bg-[#353535] hover:bg-[#234689] rounded-3xl">
                             <div className="flex flex-col items-start justify-center w-full 2xl:px-10 gap-8">
                                 <span className="text-xl text-white font-semibold">Educação integral</span>
-                                <p>Nosso propósito é formar mais do que intelectos, mas servos do Reino. Além das aulas, há encontros de tutoria para acompanhar e ajudar na caminhada de aprendizagem e formação.</p>
+                                <p className="text-white">Nosso propósito é formar mais do que intelectos, mas servos do Reino. Além das aulas, há encontros de tutoria para acompanhar e ajudar na caminhada de aprendizagem e formação.</p>
                             </div>
                         </article>
                         <article className="flex flex-col items-center justify-center lg:justify-start w-full lg:w-[calc(100%/4-30px)] lg:min-h-[308px] gap-10 py-10 p-4 bg-[#353535] hover:bg-[#234689] rounded-3xl">
                             <div className="flex flex-col items-start justify-center w-full 2xl:px-10 gap-8">
                                 <span className="text-xl text-white font-semibold">Educação Eclesiástica</span>
-                                <p>Nosso foco é a formação para edificação das igrejas locais. Toda disciplina e aprendizado busca levar os alunos ao serviço na igreja.</p>
+                                <p className="text-white">Nosso foco é a formação para edificação das igrejas locais. Toda disciplina e aprendizado busca levar os alunos ao serviço na igreja.</p>
                             </div>
                         </article>
                         <article className="flex flex-col items-center justify-center lg:justify-start w-full lg:w-[calc(100%/4-30px)] lg:min-h-[308px] gap-10 py-10 p-4 bg-[#353535] hover:bg-[#234689] rounded-3xl">
                             <div className="flex flex-col items-start justify-center w-full 2xl:px-10 gap-8">
                                 <span className="text-xl text-white font-semibold">Educação Pública</span>
-                                <p>Uma formação para que cristãos possam assumir os pressupostos bíblicos em todas as áreas do saber (exatadas, biológicas, saúde, humanas etc).</p>
+                                <p className="text-white">Uma formação para que cristãos possam assumir os pressupostos bíblicos em todas as áreas do saber (exatadas, biológicas, saúde, humanas etc).</p>
                             </div>
                         </article>
                     </div>
